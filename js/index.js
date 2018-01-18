2a52dda8fab9d1b62bae887d52fef1c02a5d099c
AbinashGupta <abinash.gupta007@gmail.com>
2018-01-18 23:00:49 +0530

    changes to project links

--- js/index.js
+++ js/index.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
     /* smooth scrolling for nav sections */
     $('#nav .navbar-nav li>a').click(function(){
       var link = $(this).attr('href');
-      var posi = $(link).offset().top+20;
+      var posi = $(link).offset().top;
       $('body,html').animate({scrollTop:posi},700);
     })
 
